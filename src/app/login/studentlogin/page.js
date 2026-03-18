a2704da4509ced3a300b0f03c4facf33b0b67e97
mohdjagir <zakirmohd547@gmail.com>
2026-03-18 15:17:46 +0530

    updated text

--- src/app/login/studentlogin/page.js
+++ src/app/login/studentlogin/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function StudentLogin() {
   return (
     <div>
-    <h1>Student Login Page</h1>
+    <h1>Student Login Page Details</h1>
     <Link href={"/login"}>go to login</Link>
     </div>
   )
